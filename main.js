44a4796a6796a653db215aa4149c4a190984f72b
Marcos Iannibelli <mdiannibelli@gmail.com>
2022-07-07 22:38:39 -0300

    Header + Body slider w/js

---
+++ main.js
@@ -0,0 +1,23 @@
+// Slider
+
+var Slider = function() {
+    const sliders = document.querySelectorAll(".slider__body");
+    const arrowNext = document.querySelector("#next");
+    const arrowBefore = document.querySelector("#before");
+    var value; 
+    arrowNext.addEventListener("click", ()=>changePosition(1));
+    arrowBefore.addEventListener("click", ()=>changePosition(-1));
+
+    function changePosition(change) { // obtengo el elemento actual junto a su data-id
+        const currentElement = Number(document.querySelector(".slider__body--show").dataset.id);
+        value = currentElement;
+        value+= change;
+
+        if(value === 0 || value == sliders.length+1) {
+            value = value === 0 ? sliders.length : 1;
+        }
+
+        sliders[value-1].classList.toggle("slider__body--show");
+        sliders[currentElement-1]. classList.toggle("slider__body--show");
+    }
+}
