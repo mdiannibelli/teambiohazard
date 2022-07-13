43a6574504bbbe2fd370b04dfabf89ed74f2e8d8
Marcos Iannibelli <mdiannibelli@gmail.com>
2022-07-13 19:03:22 -0300

    Starting cart js

---
+++ cart.js
@@ -0,0 +1,91 @@
+// Cart 
+
+const items = [
+
+    {
+        "id": 1,
+        "name": "Main Shirt",
+        "img": "./imgs/Shirt.jpg",
+        "price": 20.00,
+        "amount": 1,
+    },
+    {
+        "id": 2,
+        "name": "Face Mask",
+        "img": "./imgs/Facemask.jpg",
+        "price": 4.00,
+        "amount": 1,
+    },
+    {
+        "id": 3,
+        "name": "Wooden Cap",
+        "img": "./imgs/Wooden cap.jpg",
+        "price": 7.50,
+        "amount": 1,
+    },
+    {
+        "id": 4,
+        "name": "Cap",
+        "img": "./imgs/Cap.jpg",
+        "price": 8.00,
+        "amount": 1,
+    },
+    {
+        "id": 5,
+        "name": "Sport Hoodie",
+        "img": "./imgs/SportHoodie.jpg",
+        "price": 25.00,
+        "amount": 1,
+    },
+    {
+        "id": 6,
+        "name": "Red Hoodie",
+        "img": "./imgs/RedHoodie.jpg",
+        "price": 21.00,
+        "amount": 1,
+    },
+    {
+        "id": 7,
+        "name": "Black Hoodie",
+        "img": "./imgs/BlackHoodie.jpg",
+        "price": 21.00,
+        "amount": 1,
+    },
+    {
+        "id": 8,
+        "name": "White Hoodie",
+        "img": "./imgs/WhiteHoodie.jpg",
+        "price": 21.00,
+        "amount": 1,
+    },
+    {
+        "id": 9,
+        "name": "Jacket",
+        "img": "./imgs/Jacket.jpg",
+        "price": 46.00,
+        "amount": 1,
+    },
+    {
+        "id": 10,
+        "name": "Drink Ware",
+        "img": "./imgs/Drinkware.jpg",
+        "price": 5.50,
+        "amount": 1,
+    },
+    {
+        "id": 11,
+        "name": "Keychain",
+        "img": "./imgs/Keychain.jpg",
+        "price": 2.00,
+        "amount": 1,
+    },
+    {
+        "id": 12,
+        "name": "Pin",
+        "img": "./imgs/Pin.jpg",
+        "price": 1.50,
+        "amount": 1,
+    }
+    
+]
+
