81c1a970a8c699edfab310518ef23f16ab395bda
Marcos Iannibelli <mdiannibelli@gmail.com>
2022-07-28 20:11:42 -0300

    agrego data.json

--- cart.js
+++ cart.js
@@ -1,261 +1,10 @@
 // Cart 
-
-const items = [
-
-    {
-        "id": 1,
-        "name": "Main Shirt",
-        "img": "./imgs/Shirt.jpg",
-        "price": 20.00,
-        "amount": 1,
-        "content": "hiddencontent",
-        "hide": `
-        <h3>SIZE</h3>
-        <h3>H</h3>
-        <h3>W</h3>
-        <h4>S</h4>
-        <h4>64cm</h4>
-        <h4>44cm</h4>
-        <h4>M</h4>
-        <h4>66cm</h4>
-        <h4>46cm</h4>
-        <h4>L</h4>
-        <h4>68cm</h4>
-        <h4>49cm</h4>
-        <h4>XL</h4>
-        <h4>70cm</h4>
-        <h4>52cm</h4>
-        <h4>XXL</h4>
-        <h4>75cm</h4>
-        <h4>56cm</h4>
-        `
-    },
-    {
-        "id": 2,
-        "name": "Face Mask",
-        "img": "./imgs/Facemask.jpg",
-        "price": 4.00,
-        "amount": 1,
-        "content": "hiddencontent2",
-        "hide": `
-        <h4>Stay safe and cool with an BZ face mask.</h4>
-        <h4>100% Polyester inlayed with cotton</h4>
-        <h4>Adjustable string straps on each size</h4>
-        <h4>One size fits all</h4>
-        <h4>Stays on while talking</h4>
-        `
-    },
-    {
-        "id": 3,
-        "name": "Wooden Cap",
-        "img": "./imgs/Wooden cap.jpg",
-        "price": 7.50,
-        "amount": 1,
-        "content": "hiddencontent2",
-        "hide": `
-        <h4>Keep your head warm by this wooden cap in pure sunflower softness!</h4>
-        <h4>Everyone deserves its own daily dose of OG love. They’re so comfy!</h4>
-        <h4>Made with cotton</h4>
-        <h4>Universal size</h4>
-        `
-    },
-    {
-        "id": 4,
-        "name": "Cap",
-        "img": "./imgs/Cap.jpg",
-        "price": 8.00,
-        "amount": 1,
-        "content": "hiddencontent2",
-        "hide": `
-        <h4>Enjoy our cap that we use to play in summer!</h4>
-        <h4>Laser perforations strategically optimize breathability on the front and side panels</h4>
-        <h4>Made with recycled polyester fibers</h4>
-        <h4>Adjustable size</h4>
-        `
-    },
-    {
-        "id": 5,
-        "name": "Sport Hoodie",
-        "img": "./imgs/SportHoodie.jpg",
-        "price": 25.00,
-        "amount": 1,
-        "content": "hiddencontent",
-        "hide": `
-        <h3>SIZE</h3>
-        <h3>H</h3>
-        <h3>W</h3>
-        <h4>S</h4>
-        <h4>68cm</h4>
-        <h4>56cm</h4>
-        <h4>M</h4>
-        <h4>70cm</h4>
-        <h4>58cm</h4>
-        <h4>L</h4>
-        <h4>73cm</h4>
-        <h4>60cm</h4>
-        <h4>XL</h4>
-        <h4>76cm</h4>
-        <h4>62cm</h4>
-        <h4>XXL</h4>
-        <h4>79cm</h4>
-        <h4>64cm</h4>
-        `
-    },
-    {
-        "id": 6,
-        "name": "Red Hoodie",
-        "img": "./imgs/RedHoodie.jpg",
-        "price": 21.00,
-        "amount": 1,
-        "content": "hiddencontent",
-        "hide": `
-        <h3>SIZE</h3>
-        <h3>H</h3>
-        <h3>W</h3>
-        <h4>S</h4>
-        <h4>68cm</h4
-        <h4>56cm</h4>
-        <h4>M</h4>
-        <h4>70cm</h4>
-        <h4>58cm</h4>
-        <h4>L</h4>
-        <h4>73cm</h4>
-        <h4>60cm</h4>
-        <h4>XL</h4>
-        <h4>76cm</h4>
-        <h4>62cm</h4>
-        <h4>XXL</h4>
-        <h4>79cm</h4>
-        <h4>64cm</h4>
-        `
-    },
-    {
-        "id": 7,
-        "name": "Black Hoodie",
-        "img": "./imgs/BlackHoodie.jpg",
-        "price": 21.00,
-        "amount": 1,
-        "content": "hiddencontent",
-        "hide": `
-        <h3>SIZE</h3>
-        <h3>H</h3>
-        <h3>W</h3>
-        <h4>S</h4>
-        <h4>68cm</h4>
-        <h4>56cm</h4>
-        <h4>M</h4>
-        <h4>70cm</h4>
-        <h4>58cm</h4>
-        <h4>L</h4>
-        <h4>73cm</h4>
-        <h4>60cm</h4>
-        <h4>XL</h4>
-        <h4>76cm</h4>
-        <h4>62cm</h4>
-        <h4>XXL</h4>
-        <h4>79cm</h4>
-        <h4>64cm</h4>
-        `
-    },
-    {
-        "id": 8,
-        "name": "White Hoodie",
-        "img": "./imgs/WhiteHoodie.jpg",
-        "price": 21.00,
-        "amount": 1,
-        "content": "hiddencontent",
-        "hide": `
-        <h3>SIZE</h3>
-        <h3>H</h3>
-        <h3>W</h3>
-        <h4>S</h4>
-        <h4>68cm</h4>
-        <h4>56cm</h4>
-        <h4>M</h4>
-        <h4>70cm</h4>
-        <h4>58cm</h4>
-        <h4>L</h4>
-        <h4>73cm</h4>
-        <h4>60cm</h4>
-        <h4>XL</h4>
-        <h4>76cm</h4>
-        <h4>62cm</h4>
-        <h4>XXL</h4>
-        <h4>79cm</h4>
-        <h4>64cm</h4>
-        `
-    },
-    {
-        "id": 9,
-        "name": "Jacket",
-        "img": "./imgs/Jacket.jpg",
-        "price": 46.00,
-        "amount": 1,
-        "content": "hiddencontent",
-        "hide": `
-        <h3>SIZE</h3>
-        <h3>H</h3>
-        <h3>W</h3>
-        <h4>S</h4>
-        <h4>68cm</h4>
-        <h4>56cm</h4>
-        <h4>M</h4>
-        <h4>70cm</h4>
-        <h4>58cm</h4>
-        <h4>L</h4>
-        <h4>73cm</h4>
-        <h4>60cm</h4>
-        <h4>XL</h4>
-        <h4>76cm</h4>
-        <h4>62cm</h4>
-        <h4>XXL</h4>
-        <h4>79cm</h4>
-        <h4>64cm</h4>
-        `
-    },
-    {
-        "id": 10,
-        "name": "Drink Ware",
-        "img": "./imgs/Drinkware.jpg",
-        "price": 5.50,
-        "amount": 1,
-        "content": "hiddencontent2",
-        "hide": `
-        <h4>High quality drinkware.</h4>
-        <h4>Include BZ Esports logo</h4>
-        <h4>Metal and soft PVC</h4>
-        <h4>Customizable color</h4>
-        `
-    },
-    {
-        "id": 11,
-        "name": "Keychain",
-        "img": "./imgs/Keychain.jpg",
-        "price": 2.00,
-        "amount": 1,
-        "content": "hiddencontent2",
-        "hide": `
-        <h4>High quality keychain.</h4>
-        <h4>Include BZ Esports logo</h4>
-        <h4>Metal and soft PVC</h4>
-        <h4>Customizable color</h4>
-        `
-    },
-    {
-        "id": 12,
-        "name": "Pin",
-        "img": "./imgs/Pin.jpg",
-        "price": 1.50,
-        "amount": 1,
-        "content": "hiddencontent2",
-        "hide": `
-        <h4>High quality pin.</h4>
-        <h4>Include BZ Esports logo</h4>
-        <h4>Metal and soft PVC</h4>
-        <h4>Customizable color</h4>
-        `
-    }
-]
+const API = "./data.json";
+const getData = async () => {
+        const response = await fetch(API)
+        const data = await response.json();
+   return data;
+}
 let cart = [];
 document.addEventListener("DOMContentLoaded", () => {
     renderProducts();
@@ -264,8 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 })
 
-function renderProducts() {
+const renderProducts = async () => {
     const shop = document.getElementById("shop");
+
+    const items = await getData();
     items.forEach((p) => {
         const productHTML = document.createElement("li");
         productHTML.classList.add("prodductimg");
@@ -307,7 +58,8 @@ function renderProducts() {
 
 
 
-function addProductstoCart(id) {
+const  addProductstoCart = async (id) => {
+    const items = await getData();
     let product = items.find(items => items.id === id);
     let productInCart = cart.find(items => items.id === id);
 
